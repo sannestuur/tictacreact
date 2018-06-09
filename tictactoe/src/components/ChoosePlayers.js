9e8c615998068b28758ff9bb0d665b7f1d707bca
Sanne Stuur <sanne.stuur@gmail.com>
2018-06-09 13:14:37 +0200

    added form validation

--- tictactoe/src/components/ChoosePlayers.js
+++ tictactoe/src/components/ChoosePlayers.js
@@ -1,36 +1,90 @@
 import React, { PureComponent } from "react"
-import PropTypes from "prop-types"
 import { connect } from "react-redux"
-import { Redirect, withRouter} from "react-router-dom"
 import { setPlayers } from "../actions/game"
+import { FormErrors } from "./FormErrors"
 
 class ChoosePlayers extends PureComponent {
-  static propTypes = {
-    Player1: PropTypes.string.isRequired,
-    Player2: PropTypes.string.isRequired
+  constructor(props) {
+    super(props)
+    this.state = {
+      Player1: "",
+      Player2: "",
+      formErrors: { Player1: "", Player2: "" },
+      player1HasName: false,
+      player2HasName: false,
+      differentNames: false,
+      formValid: false
+    }
   }
-
-  state = {}
-
-  // prevent automatic form submission behaviour or don't use form tag
 
   handleSubmit = () => {
     console.log(this.state)
     this.props.setPlayers(this.state.Player1, this.state.Player2)
-    this.props.history.push('/game');
+    this.props.history.push("/game")
   }
 
   handleChange = event => {
     const { name, value } = event.target
 
+    this.setState(
+      {
+        [name]: value
+      },
+      () => {
+        this.validateField(name, value)
+      }
+    )
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors
+    let player1HasName = this.state.player1HasName
+    let player2HasName = this.state.player2HasName
+    let differentNames = this.state.differentNames
+
+    switch (fieldName) {
+      case "Player1":
+        player1HasName = value.length >= 1
+        fieldValidationErrors.Player1 = player1HasName
+          ? ""
+          : "Player 1 must have a name"
+        break
+      case "Player2":
+        player2HasName = value.length >= 1
+        differentNames = this.state.Player1 !== this.state.Player2
+        if (!player2HasName) {
+          fieldValidationErrors.Player2 = "Player 2 must have a name"
+        } else if (!differentNames) {
+          fieldValidationErrors.Player2 =
+            "Player 1 and 2 must have different names"
+        } else fieldValidationErrors.Player2 = ""
+        break
+      default:
+        break
+    }
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        player1HasName: player1HasName,
+        player2HasName: player2HasName,
+        differentNames: differentNames
+      },
+      this.validateForm
+    )
+  }
+
+  validateForm() {
     this.setState({
-      [name]: value
+      formValid:
+        this.state.player1HasName &&
+        this.state.player2HasName &&
+        this.state.differentNames
     })
   }
 
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor="Player1">Player 1</label>
           <input
@@ -53,10 +107,13 @@ class ChoosePlayers extends PureComponent {
             onChange={this.handleChange}
           />
         </div>
-        <div>
+        <div className="panel panel-default">
+          <FormErrors formErrors={this.state.formErrors} />
         </div>
-        <button onClick={this.handleSubmit}>Play!</button>
-      </div>
+        <button type="submit" disabled={!this.state.formValid}>
+          Play!
+        </button>
+      </form>
     )
   }
 }
